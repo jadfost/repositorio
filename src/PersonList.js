474fb9d8bb6cc24db405d9445fb77ac5779a341e
nirttson <stevenorduz@hotmail.com>
2023-05-03 11:47:48 -0500

    V.1.1.3

--- src/PersonList.js
+++ src/PersonList.js
@@ -1,3 +1,10 @@
 import React, { useState } from 'react';
 import './PersonList.css';
 
+
+function ListaPersonas() {
+  const [personas, setPersonas] = useState([]);
+ 
+  }
+  
+  export default ListaPersonas;
