92c85f90556e6597282e4b601e4f83cc1f648642
nirttson <stevenorduz@hotmail.com>
2023-05-03 11:48:08 -0500

    V1.1.4

--- src/PersonList.js
+++ src/PersonList.js
@@ -4,7 +4,12 @@ import './PersonList.css';
 
 function ListaPersonas() {
   const [personas, setPersonas] = useState([]);
- 
+  
+  const agregarPersona = (persona) => {
+  setPersonas([...personas, persona]);
+  }
+  
+  
   }
   
   export default ListaPersonas;
