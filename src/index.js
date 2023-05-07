0119b48847d0e7b76c27aaa0a2f5c534326003ef
stevenorduz <stevenorduz@hotmail.com>
2023-05-06 19:39:14 -0500

    V5.1.1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import PersonListPage from './Pages/PersonListPage';
 import reportWebVitals from './reportWebVitals';
-import PersonList from './PersonList'; // 
 
 ReactDOM.render(
-    <React.StrictMode>
-      <PersonList />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-  
+  <React.StrictMode>
+    <PersonListPage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
 reportWebVitals();
