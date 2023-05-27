7e7eecc4b2cba286bfbac8630da30cdb6fe2e7b5
Jared Foster <stevenorduz@hotmail.com>
2023-05-27 11:29:53 -0500

    V7.8.1

--- src/organismos/ListaPersonas.jsx
+++ src/organismos/ListaPersonas.jsx
@@ -4,7 +4,14 @@ import Tabla from '../moleculas/Tabla';
 import './ListaPersonas.css';
 
 const ListaPersonas = () => {
-  const [personas, setPersonas] = useState([]);
+  const [personas, setPersonas] = useState([
+    { id: 1, nombre: 'Maria', apellido: 'Carvajal', edad: 30, numeroIdentificacion: '12345678' },
+    { id: 2, nombre: 'Jonathan', apellido: 'Muñoz', edad: 25, numeroIdentificacion: '87654321' },
+    { id: 3, nombre: 'Daniel', apellido: 'Torres', edad: 2, numeroIdentificacion: '87654321' },
+    { id: 4, nombre: 'Maria', apellido: 'Carvajal', edad: 30, numeroIdentificacion: '12345678' },
+    { id: 5, nombre: 'Jonathan', apellido: 'Muñoz', edad: 25, numeroIdentificacion: '87654321' },
+    { id: 6, nombre: 'Daniel', apellido: 'Torres', edad: 2, numeroIdentificacion: '87654321' },
+  ]);
   const [mostrarFormulario, setMostrarFormulario] = useState(false);
 
   const agregarPersona = (persona) => {
@@ -23,7 +30,7 @@ const ListaPersonas = () => {
     <div>
       <h1 className="person-list-heading">Lista de personas</h1>
       <button className={`button-toggle ${mostrarFormulario ? 'show' : ''}`} onClick={toggleFormulario}>
-       + AGREGAR NUEVA PERSONA
+        + AGREGAR NUEVA PERSONA
       </button>
       {mostrarFormulario && <Formulario onSubmit={agregarPersona} />}
       <Tabla data={personas} onDelete={eliminarPersona} />
